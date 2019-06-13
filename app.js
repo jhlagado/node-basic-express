4a8b280c5996b93eadc93f37b2dec16542e22140
hurricanew <hurricanew@hotmail.com>
2019-06-13 13:47:04 +0000

    mail sending finish 5

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var nodemailer = require('nodemailer');
+var nodeMailer = require('nodemailer');
 
 var app = express();
 
@@ -17,5 +17,42 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=>{
     res.render('about');
 })
+app.get('/contact',(req,res)=>{
+    res.render('contact');
+})
+app.post('/contact/send', (req, res) => {
+  console.log('Sucessfully posted');
+  //transporter
+  var transporter = nodeMailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'your email address',
+      pass: 'your password'
+    }
+  });
+
+  var mailOptions = {
+    from: 'Marco del Carmen <jmdc@my.codergv.org>',
+    to: req.body.email,
+    subject: 'Nodemailer Testing',
+    text: `You have a submission with the following details... Name: ${req.body.name} Email: ${req.body.email} Message: ${req.body.message}`,
+    html: `<p>You have a submission with the following details...</p>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Message: ${req.body.message}</li>
+    </ul>`
+  };
+
+  transporter.sendMail(mailOptions, (e, info) => {
+    if(e) {
+      console.log(e);
+      res.redirect('/');
+    } else {
+      console.log(`Message Sent: ${info.response}`);
+      res.redirect('/');
+    }
+  });
+});
 app.listen(3000);
 console.log('server is running on port 3000');
