12c90172a194cffab77d4fc2df5ee6f9543c0ffd
hurricanew <hurricanew@hotmail.com>
2019-06-13 12:47:18 +0000

    jade rendering grammar 2

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
 app.listen(3000);
 console.log('server is running on port 3000');
