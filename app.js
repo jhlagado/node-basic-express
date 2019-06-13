446792873f8dcadab76314fcdd19a10df7e50d26
hurricanew <hurricanew@hotmail.com>
2019-06-13 13:13:57 +0000

    bootstrap css page ready

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res)=>{
     res.render('index');
